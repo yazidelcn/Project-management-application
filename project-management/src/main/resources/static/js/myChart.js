0712aa71b2109f65e7044fe8dbb787c66662cfed
Yazid Elcouhen <elcouhenyazid@gmail.com>
2021-10-08 19:31:03 +0100

    enhancing the home page and adding chartJs

---
+++ project-management/src/main/resources/static/js/myChart.js
@@ -0,0 +1,15 @@
+var myPieChart = new Chart(document.getElementById("myPieChart"),{
+    type: 'pie',
+     data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
